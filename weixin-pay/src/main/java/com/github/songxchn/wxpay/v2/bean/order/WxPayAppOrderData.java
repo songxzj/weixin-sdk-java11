16c180b88f9c7cefa9f3715a6471e29a82751640
songx <980343313@qq.com>
2021-01-08 14:36:36 +0800

    拉起支付对象

--- weixin-pay/src/main/java/com/github/songxchn/wxpay/v2/bean/order/WxPayAppOrderData.java
+++ weixin-pay/src/main/java/com/github/songxchn/wxpay/v2/bean/order/WxPayAppOrderData.java
@@ -2,9 +2,12 @@ package com.github.songxchn.wxpay.v2.bean.order;
 
 
 import cn.hutool.core.util.RandomUtil;
+import com.github.songxchn.common.exception.WxErrorException;
+import com.github.songxchn.common.exception.WxErrorExceptionFactor;
+import com.github.songxchn.wxpay.util.SignUtils;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
-import lombok.Builder;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.Serializable;
 
@@ -12,26 +15,118 @@ import java.io.Serializable;
  * APP支付
  */
 @Data
-@Builder
 public class WxPayAppOrderData implements Serializable {
     private static final long serialVersionUID = -3997925111530175505L;
 
+    /**
+     * 应用id
+     * appid
+     * string[1,32]
+     * 是
+     */
     private String appid;
+
+    /**
+     * 商户号
+     * partnerid
+     * string[1,32]
+     * 是
+     */
     private String partnerid;
+
+    /**
+     * 预支付交易会话ID
+     * prepayid
+     * string[1,64]
+     * 是
+     */
     private String prepayid;
 
     /**
+     * 扩展字段
+     * package
+     * string[1,128]
+     * 是
      * 由于package为java保留关键字，因此改为packageValue. 前端使用时记得要更改为package
      */
-    @Builder.Default
     @XStreamAlias("package")
-    private String packageValue = "Sign=WXPay";
+    private String packageValue;
 
-    @Builder.Default
-    private String noncestr  = RandomUtil.randomString(32);
+    /**
+     * 随机字符串
+     * noncestr
+     * String(32)
+     * 是
+     */
+    private String noncestr;
 
-    @Builder.Default
-    private String timestamp = String.valueOf(System.currentTimeMillis() / 1000);
+    /**
+     * 时间戳
+     * timestamp
+     * String(10)
+     * 是
+     */
+    private String timestamp;
 
+    /**
+     * 签名
+     * sign
+     * String(64)
+     * 是
+     */
     private String sign;
+
+    private WxPayAppOrderData(String appid, String partnerid, String prepayid) {
+        this.appid = appid;
+        this.partnerid = partnerid;
+        this.prepayid = prepayid;
+        this.packageValue = "Sign=WXPay";
+        this.noncestr = RandomUtil.randomString(32);
+        this.timestamp = String.valueOf(System.currentTimeMillis() / 1000);
+    }
+
+    public static class WxPayAppOrderDataBuilder {
+
+        private String appid;
+
+        private String partnerid;
+
+        private String prepayid;
+
+        private String signType;
+
+        public WxPayAppOrderDataBuilder appid(String appid) {
+            this.appid = appid;
+            return this;
+        }
+
+        public WxPayAppOrderDataBuilder partnerid(String partnerid) {
+            this.partnerid = partnerid;
+            return this;
+        }
+
+        public WxPayAppOrderDataBuilder prepayid(String prepayid) {
+            this.prepayid = prepayid;
+            return this;
+        }
+
+        public WxPayAppOrderData buildWithMchKey(String mchKey) throws WxErrorException {
+            if (StringUtils.isBlank(this.appid)) {
+                throw new WxErrorException(WxErrorExceptionFactor.INVALID_PARAMETER_CODE, "appid 必须提供值");
+            }
+            if (StringUtils.isBlank(this.partnerid)) {
+                throw new WxErrorException(WxErrorExceptionFactor.INVALID_PARAMETER_CODE, "partnerid 必须提供值");
+            }
+            if (StringUtils.isBlank(this.prepayid)) {
+                throw new WxErrorException(WxErrorExceptionFactor.INVALID_PARAMETER_CODE, "prepayid 必须提供值");
+            }
+            if (StringUtils.isBlank(this.signType)) {
+                throw new WxErrorException(WxErrorExceptionFactor.INVALID_PARAMETER_CODE, "signType 必须提供值");
+            }
+            WxPayAppOrderData data = new WxPayAppOrderData(this.appid, this.partnerid, this.prepayid);
+            data.sign = SignUtils.createSign(data, this.signType, mchKey, new String[0]);
+
+            return data;
+        }
+    }
 }
---
+++ weixin-pay/src/main/java/com/github/songxchn/wxpay/v2/bean/order/WxPayJsapiOrderData.java
@@ -0,0 +1,122 @@
+package com.github.songxchn.wxpay.v2.bean.order;
+
+import cn.hutool.core.util.RandomUtil;
+import com.github.songxchn.common.exception.WxErrorException;
+import com.github.songxchn.common.exception.WxErrorExceptionFactor;
+import com.github.songxchn.wxpay.util.SignUtils;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.Serializable;
+
+/**
+ * JSAPI/小程序支付
+ */
+@Data
+public class WxPayJsapiOrderData implements Serializable {
+    private static final long serialVersionUID = -1596569775320792912L;
+
+    /**
+     * 公众号id
+     * appId
+     * 是
+     * String(16)
+     */
+    private String appId;
+
+    /**
+     * 时间戳
+     * timeStamp
+     * 是
+     * String(32)
+     */
+    private String timeStamp;
+
+    /**
+     * 随机字符串
+     * nonceStr
+     * 是
+     * String(32)
+     */
+    private String nonceStr;
+    /**
+     * /**
+     * 订单详情扩展字符串
+     * package
+     * 是
+     * String(128)
+     * 由于package为java保留关键字，因此改为packageValue. 前端使用时记得要更改为package
+     */
+    @XStreamAlias("package")
+    private String packageValue;
+
+    /**
+     * 签名方式
+     * signType
+     * 是
+     * String(32)
+     */
+    private String signType;
+
+    /**
+     * 签名
+     * paySign
+     * 是
+     * String(64)
+     */
+    private String paySign;
+
+    private WxPayJsapiOrderData(String appId, String prepayId, String signType) {
+        this.appId = appId;
+        this.timeStamp = String.valueOf(System.currentTimeMillis() / 1000);
+        this.nonceStr = RandomUtil.randomString(32);
+        this.packageValue = "prepay_id=" + prepayId;
+        this.signType = signType;
+    }
+
+    public static WxPayJsapiOrderDataBuilder newBuilder() {
+        return new WxPayJsapiOrderDataBuilder();
+    }
+
+    public static class WxPayJsapiOrderDataBuilder {
+
+        private String appId;
+
+        private String prepayId;
+
+        private String signType;
+
+        public WxPayJsapiOrderDataBuilder appId(String appId) {
+            this.appId = appId;
+            return this;
+        }
+
+        public WxPayJsapiOrderDataBuilder prepayId(String prepayId) {
+            this.prepayId = prepayId;
+            return this;
+        }
+
+        public WxPayJsapiOrderDataBuilder signType(String signType) {
+            this.signType = signType;
+            return this;
+        }
+
+        public WxPayJsapiOrderData buildWithMchKey(String mchKey) throws WxErrorException {
+            if (StringUtils.isBlank(this.appId)) {
+                throw new WxErrorException(WxErrorExceptionFactor.INVALID_PARAMETER_CODE, "appId 必须提供值");
+            }
+            if (StringUtils.isBlank(this.prepayId)) {
+                throw new WxErrorException(WxErrorExceptionFactor.INVALID_PARAMETER_CODE, "prepayId 必须提供值");
+            }
+            if (StringUtils.isBlank(this.signType)) {
+                throw new WxErrorException(WxErrorExceptionFactor.INVALID_PARAMETER_CODE, "signType 必须提供值");
+            }
+            WxPayJsapiOrderData data = new WxPayJsapiOrderData(this.appId, this.prepayId, this.signType);
+            data.paySign = SignUtils.createSign(data, data.getSignType(), mchKey, new String[0]);
+
+            return data;
+        }
+    }
+
+}
