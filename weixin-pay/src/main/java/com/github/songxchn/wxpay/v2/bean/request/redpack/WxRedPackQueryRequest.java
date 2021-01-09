a390c89d3eed7d8d7fff0bbd3535506306ca3b88
songx <980343313@qq.com>
2021-01-09 16:51:52 +0800

    增加文档地址

--- weixin-pay/src/main/java/com/github/songxchn/wxpay/v2/bean/request/redpack/WxRedPackQueryRequest.java
+++ weixin-pay/src/main/java/com/github/songxchn/wxpay/v2/bean/request/redpack/WxRedPackQueryRequest.java
@@ -11,6 +11,13 @@ import lombok.NoArgsConstructor;
 
 import java.util.Map;
 
+/**
+ * 查询红包记录
+ * 普通商户
+ * <a href="https://pay.weixin.qq.com/wiki/doc/api/tools/cash_coupon.php?chapter=13_6&index=5">
+ * 服务商
+ * <a href="https://pay.weixin.qq.com/wiki/doc/api/tools/cash_coupon_sl.php?chapter=13_6&index=5">
+ */
 @Data
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
